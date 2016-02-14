005ab8c1d61dc35781de343d00469d9be387a089
pawsingh <pawan.singh@digitaslbi.com>
2016-02-14 12:54:15 +0000

    added tab as selected

---
+++ Demo1/Common/BooleanToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Demo1.Common
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Brushes.Aqua : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
